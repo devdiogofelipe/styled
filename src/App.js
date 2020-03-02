25d76f84c9f2e2e1fe744370ade14d80663dc641
Diogo Felipe <devdiogofelipe@outlook.com>
2020-03-02 16:48:20 -0300

    utilizando styled components pela primeira vez

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Title, TitleSmall } from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title fontSize={20}>
+          Hello World
+          <span>Texto menor</span>
+        </Title>
+
+        <TitleSmall></TitleSmall>
+      </div>
+    )
+  }
 }
 
 export default App;
